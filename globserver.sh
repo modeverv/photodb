add2e8731e8df92167b8119ef6335aec84308748
modeverv <modeverv@gmail.com>
2016-03-14 11:01:09 +0900

    rvm

--- globserver.sh
+++ globserver.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 cd /home/seijiro/sinatra/photodb
+export PATH="/home/seijiro/.rvm/bin:$PATH" # Add RVM to PATH for scripting
 #source /home/seijiro/.rvm/environments/ruby-1.9.3-p392
+source /home/seijiro/.rvm/environments/ruby-1.9.3-p551
 # bundle exec ruby ./update_server.rb
 bundle exec ruby ./glob_server.rb
